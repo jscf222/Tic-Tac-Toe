12aa1b72ef4e5f4260f41c08ad073be0e1c75114
jscf222 <jscf222@gmail.com>
2023-05-07 14:00:39 -0700

    adding player moves

--- TicTacToe.js
+++ TicTacToe.js
@@ -3,5 +3,7 @@ let boxes = Array.from(document.getElementsByClassName('box'))
 let resetButton = document.getElementById('resetButton')
 let playerText = document.getElementById('playerText')
 console.log(boxes)
-
-
+// making player moveslll
+let O_Text = 'O'
+let X_Text = 'X'
+let currentPlayer = X_Text
