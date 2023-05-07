e8214793b0f34aef4acad065eba0157514679da0
jscf222 <jscf222@gmail.com>
2023-05-07 13:49:38 -0700

    making array

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,4 +1,7 @@
 // making sure javascript is connected and assign box classes to an array
-
+let boxes = Array.from(document.getElementsByClassName('box'))
+let resetButton = document.getElementById('resetButton')
+let playerText = document.getElementById('playerText')
+console.log(boxes)
 
 
